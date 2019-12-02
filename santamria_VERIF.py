8bdd90a2ce3e7ee5b20a44f8e2a13208226c10c0
santamaria-olivos-unprg <57973991+santamaria-olivos-unprg@users.noreply.github.com>
2019-12-02 10:53:38 -0500

    se agrego los verificadores de santamaria olivos

---
+++ santamria_VERIF.py
@@ -0,0 +1,671 @@
+#Ejercicio 1
+paciente="Wilder"
+type_paciente=type(paciente)
+
+paciente_es_str=isinstance(paciente,str)
+paciente_es_int=isinstance(paciente,int)
+
+print("Variable paciente es:",type_paciente)
+print("Variable es str?:",paciente_es_str)
+print("Variable es int?:",paciente_es_int)
+print("FIN DEl EJERCICIO 1")
+
+
+
+#Ejercicio 2
+edad=21
+type_edad=type(edad)
+
+edad_es_int=isinstance(edad,int)
+edad_es_float=isinstance(edad,float)
+
+print("Variable edad es:",type_edad)
+print("Variable es int?:",edad_es_int)
+print("Variable es float?:",edad_es_float)
+print("FIN DEl EJERCICIO 2")
+
+
+#Ejercicio 3
+altura=1.60
+type_altura=type(altura)
+
+altura_es_float=isinstance(altura,float)
+altura_es_int=isinstance(altura,int)
+
+print("Variable altura es:",type_altura)
+print("Variable es float?:",altura_es_float)
+print("Variable es int?:",altura_es_int)
+print("FIN DEl EJERCICIO 3")
+
+
+#Ejercicio 4
+peso=55.0
+type_peso=type(peso)
+
+peso_es_float=isinstance(peso,float)
+peso_es_str=isinstance(peso,str)
+
+print("Variable eso es:",type_peso)
+print("Variable es float?:",peso_es_float)
+print("Variable es str?:",peso_es_str)
+print("FIN DEl EJERCICIO 4")
+
+
+#Ejercicio 5
+masa_corporal=21.5
+type_masa_corporal=type(masa_corporal)
+
+masa_corporal_es_float=isinstance(masa_corporal,float)
+masa_corporal_es_int=isinstance(masa_corporal,int)
+
+print("Variable masa corporal es:",type_masa_corporal)
+print("Variable es float?:",masa_corporal_es_float)
+print("Variable es int?:",masa_corporal_es_int)
+print("FIN DEl EJERCICIO 5")
+
+
+#Ejercicio 6
+alumno="Robert"
+type_alumno=type(alumno)
+
+alumno_es_str=isinstance(edad,str)
+alumno_es_bool=isinstance(edad,bool)
+
+print("Variable alumno es:",type_alumno)
+print("Variable es srt?:",alumno_es_str)
+print("Variable es bool?:",alumno_es_bool)
+print("FIN DEl EJERCICIO 6")
+
+
+#Ejercicio 7
+nota1=17
+type_nota1=type(nota1)
+
+nota1_es_int=isinstance(nota1,int)
+nota1_es_bool=isinstance(nota1,bool)
+
+print("Variable nota1 es:",type_nota1)
+print("Variable es int?:",nota1_es_int)
+print("Variable es bool?:",nota1_es_bool)
+print("FIN DEl EJERCICIO 7")
+
+
+#Ejercicio 8
+nota2=12
+type_nota2=type(nota2)
+
+nota2_es_int=isinstance(nota2,int)
+nota2_es_float=isinstance(nota2,float)
+
+print("Variable nota2 es:",type_nota2)
+print("Variable es int?:",nota2_es_int)
+print("Variable es float?:",nota2_es_float)
+print("FIN DEl EJERCICIO 8")
+
+#Ejercicio 9
+nota3=20
+type_nota3=type(nota3)
+
+nota3_es_int=isinstance(nota3,int)
+nota3_es_bool=isinstance(nota3,bool)
+
+print("Variable nota3 es:",type_nota3)
+print("Variable es int?:",nota3_es_int)
+print("Variable es bool?:",nota3_es_bool)
+print("FIN DEl EJERCICIO 9")
+
+#Ejercicio 10
+promedio=16
+type_promedio=type(promedio)
+
+promedio_es_int=isinstance(promedio,int)
+promedio_es_float=isinstance(promedio,float)
+
+print("Variable promedio es:",type_promedio)
+print("Variable es int?:",promedio_es_int)
+print("Variable es float?:",promedio_es_float)
+print("FIN DEl EJERCICIO 10")
+
+#Ejercicio 11
+producto=""
+type_producto=type(producto)
+
+producto_es_str=isinstance(producto,str)
+producto_es_bool=isinstance(producto,bool)
+
+print("Variable producto es:",type_producto)
+print("Variable es str?:",producto_es_str)
+print("Variable es bool?:",producto_es_bool)
+print("FIN DEl EJERCICIO 11")
+
+
+#Ejercicio 12
+inventario_inicial=75
+type_inventario_inicial=type(inventario_inicial)
+
+inventario_inicial_es_float=isinstance(inventario_inicial,float)
+inventario_inicial_es_int=isinstance(inventario_inicial,int)
+
+print("Variable inventario inicial es:",type_inventario_inicial)
+print("Variable es float?:",inventario_inicial_es_float)
+print("Variable es int?:",inventario_inicial_es_int)
+print("FIN DEl EJERCICIO 12")
+
+
+#Ejercicio 13
+compras=120.750
+type_compras=type(compras)
+
+compras_es_float=isinstance(compras,float)
+compras_es_str=isinstance(compras,str)
+
+print("Variable compras es:",type_compras)
+print("Variable es float?:",compras_es_float)
+print("Variable es str?:",compras_es_str)
+print("FIN DEl EJERCICIO 13")
+
+
+#Ejercicio 14
+inventario_final=40.250
+type_inventario_final=type(inventario_final)
+
+inventario_final_es_float=isinstance(inventario_final,float)
+inventario_final_es_bool=isinstance(inventario_final,bool)
+
+print("Variable inventario final es:",type_inventario_final)
+print("Variable es float?:",inventario_inicial_es_float)
+print("Variable es bool?:",inventario_final_es_bool)
+print("FIN DEl EJERCICIO 14")
+
+
+#Ejercicio 15
+costo_ventas=155.5
+type_costo_ventas=type(costo_ventas)
+
+costo_ventas_es_float=isinstance(costo_ventas,float)
+costo_ventas_es_bool=isinstance(costo_ventas,bool)
+
+print("Variable costo de ventas es:",type_costo_ventas)
+print("Variable es float?:",costo_ventas_es_float)
+print("Variable es bool?:",costo_ventas_es_bool)
+print("FIN DEl EJERCICIO 15")
+
+
+#Ejercicio 16
+promedio_biologia=12
+type_promedio_biologia=type(promedio_biologia)
+
+promedio_biologia_es_int=isinstance(promedio_biologia,int)
+promedio_biologia_es_str=isinstance(promedio_biologia,str)
+
+print("Variable promedio de biologia es:",type_promedio_biologia)
+print("Variable es int?:",promedio_biologia_es_int)
+print("Variable es str?:",promedio_biologia_es_str)
+print("FIN DEl EJERCICIO 16")
+
+
+#Ejercicio 17
+promedio_quimica=10
+type_promedio_quimica=type(promedio_quimica)
+
+promedio_quimica_es_int=isinstance(promedio_quimica,int)
+promedio_quimica_es_bool=isinstance(promedio_quimica,bool)
+
+print("Variable promedio de quimica es:",type_promedio_quimica)
+print("Variable es int?:",promedio_quimica_es_int)
+print("Variable es bool?:",promedio_quimica_es_bool)
+print("FIN DEl EJERCICIO 17")
+
+
+#Ejercicio 18
+promedio_raz_verbal=16
+type_promedio_raz_verbal=type(promedio_raz_verbal)
+
+promedio_raz_verbal_es_int=isinstance(promedio_raz_verbal,int)
+promedio_raz_verbal_es_float=isinstance(promedio_raz_verbal,float)
+
+print("Variable promedio de Raz.Verbal es:",type_promedio_raz_verbal)
+print("Variable es int?:",promedio_raz_verbal_es_int)
+print("Variable es float?:",promedio_raz_verbal_es_float)
+print("FIN DEl EJERCICIO 18")
+
+
+#Ejercicio 19
+promedio_raz_mat=10
+type_promedio_raz_mat=type(promedio_raz_mat)
+
+promedio_raz_mat_es_int=isinstance(promedio_raz_mat,int)
+promedio_raz_mat_es_str=isinstance(promedio_raz_mat,str)
+
+print("Variable promedio de Raz.Mat es:",type_promedio_raz_mat)
+print("Variable es int?:",promedio_raz_mat_es_int)
+print("Variable es str?:",promedio_raz_mat_es_str)
+print("FIN DEl EJERCICIO 19")
+
+
+#Ejercicio 20
+promedio_general=15
+type_promedio_general=type(promedio_general)
+
+promedio_general_es_int=isinstance(promedio_general,int)
+promedio_general_es_bool=isinstance(promedio_general,bool)
+
+print("Variable promedio general es:",type_promedio_general)
+print("Variable es int?:",promedio_general_es_int)
+print("Variable es bool?:",promedio_general_es_bool)
+print("FIN DEl EJERCICIO 20")
+
+
+#Ejercicio 21
+libro_autoria_t=250.50
+type_libro_autoria_t=type(libro_autoria_t)
+
+libro_autoria_t_es_float=isinstance(libro_autoria_t,float)
+libro_autoria_t_es_int=isinstance(libro_autoria_t,int)
+
+print("Variable libro autoria t es:",type_libro_autoria_t)
+print("Variable es float?:",libro_autoria_t_es_float)
+print("Variable es int?:",libro_autoria_t_es_int)
+print("FIN DEl EJERCICIO 21")
+
+
+#Ejercicio 22
+libro_manual_c=300
+type_libro_manual_c=type(libro_manual_c)
+
+libro_manual_c_es_float=isinstance(libro_manual_c,float)
+libro_manual_c_es_str=isinstance(libro_manual_c,str)
+
+print("Variable libro manual c es:",type_libro_manual_c)
+print("Variable es float?:",libro_manual_c_es_float)
+print("Variable es str?:",libro_manual_c_es_str)
+print("FIN DEl EJERCICIO 22")
+
+
+#Ejercicio 23
+compendio_l=150.4
+type_compendio_l=type(compendio_l)
+
+compendio_l_es_float=isinstance(compendio_l,float)
+compendio_l_es_bool=isinstance(compendio_l,bool)
+
+print("Variable compendio l es:",type_compendio_l)
+print("Variable es float?:",compendio_l_es_float)
+print("Variable es str?:",compendio_l_es_bool)
+print("FIN DEl EJERCICIO 23")
+
+
+#Ejercicio 24
+total_pago=430
+type_total_pago=type(total_pago)
+
+total_pago_es_float=isinstance(total_pago,float)
+total_pago_es_int=isinstance(total_pago,int)
+
+print("Variable total pago es:",type_total_pago)
+print("Variable es float?:",total_pago_es_float)
+print("Variable es int?:",total_pago_es_int)
+print("FIN DEl EJERCICIO 24")
+
+
+#Ejercicio 25
+total_base_imp=55.0
+type_total_base_imp=type(total_base_imp)
+
+total_base_impo_es_float=isinstance(total_base_imp,float)
+total_base_imp_es_bool=isinstance(total_base_imp,bool)
+
+print("Variable total base imp es:",type_total_base_imp)
+print("Variable es float?:",total_base_impo_es_float)
+print("Variable es bool?:",total_base_imp_es_bool)
+print("FIN DEl EJERCICIO 25")
+
+
+#Ejercicio 25
+IVA21=100
+type_IVA21=type(IVA21)
+
+IVA21_es_int=isinstance(IVA21,int)
+IVA21_es_float=isinstance(IVA21,float)
+
+print("Variable IVA 21% es:",type_IVA21)
+print("Variable es int?:",IVA21_es_int)
+print("Variable es float?:",IVA21_es_float)
+print("FIN DEl EJERCICIO 25")
+
+
+#Ejercicio 26
+retencion15=85.5
+type_retencion15=type(retencion15)
+
+retencion15_es_float=isinstance(retencion15,float)
+retencion15_es_str=isinstance(retencion15,str)
+
+print("Variable retención 15% es:",type_retencion15)
+print("Variable es float?:",retencion15_es_float)
+print("Variable es str?:",retencion15_es_str)
+print("FIN DEl EJERCICIO 26")
+
+
+#Ejercicio 27
+importe_total=156.4
+type_importe_total=type(importe_total)
+
+importe_total_es_float=isinstance(importe_total,float)
+importe_total_es_int=isinstance(importe_total,int)
+
+print("Variable importe total es:",type_importe_total)
+print("Variable es float?:",importe_total_es_float)
+print("Variable es int?:",importe_total_es_int)
+print("FIN DEl EJERCICIO 27")
+
+
+#Ejercicio 28
+valor_total=156.4
+type_valor_total=type(valor_total)
+
+valor_total_es_float=isinstance(valor_total,float)
+valor_total_es_str=isinstance(valor_total,str)
+
+print("Variable valor total es:",type_valor_total)
+print("Variable es float?:",valor_total_es_float)
+print("Variable es str?:",valor_total_es_str)
+print("FIN DEl EJERCICIO 28")
+
+
+#Ejercicio 29
+tasa_IGV=450
+type_tasa_IGV=type(tasa_IGV)
+
+tasa_IGV_es_int=isinstance(tasa_IGV,int)
+tasa_IGV_es_str=isinstance(tasa_IGV,str)
+
+print("Variable tasa IGV es:",type_tasa_IGV)
+print("Variable es int?:",tasa_IGV_es_int)
+print("Variable es str?:",tasa_IGV_es_str)
+print("FIN DEl EJERCICIO 29")
+
+
+#Ejercicio 30
+valor_neto=600.5
+type_valor_neto=type(valor_neto)
+
+valor_neto_es_float=isinstance(valor_neto,float)
+valor_neto_es_bool=isinstance(valor_neto,bool)
+
+print("Variable valor neto es:",type_valor_neto)
+print("Variable es float?:",valor_neto_es_float)
+print("Variable es bool?:",valor_neto_es_bool)
+print("FIN DEl EJERCICIO 30")
+
+
+#Ejercicio 31
+IGV =500
+type_IGV=type(IGV )
+
+IGV_es_int=isinstance(IGV ,int)
+IGV_es_float=isinstance(IGV ,float)
+
+print("Variable IGV es:",type_IGV )
+print("Variable es int?:",IGV_es_int)
+print("Variable es float?:",IGV_es_float)
+print("FIN DEl EJERCICIO 31")
+
+
+#Ejercicio 32
+costo_directo=500
+type_costo_directo=type(costo_directo)
+
+costo_directo_es_int=isinstance(costo_directo ,int)
+costo_directo_es_float=isinstance(costo_directo ,float)
+
+print("Variable costo directo es:",type_costo_directo )
+print("Variable es int?:",costo_directo_es_int)
+print("Variable es float?:",costo_directo_es_float)
+print("FIN DEl EJERCICIO 32")
+
+
+#Ejercicio 33
+gasto_general_fijo=500
+type_gasto_general_fijo=type(gasto_general_fijo)
+
+gasto_general_fijo_es_int=isinstance(gasto_general_fijo,int)
+gasto_general_fijo_es_str=isinstance(gasto_general_fijo,str)
+
+print("Variable gasto general fijo es:",type_gasto_general_fijo)
+print("Variable es int?:",gasto_general_fijo_es_int)
+print("Variable es str?:",gasto_general_fijo_es_str)
+print("FIN DEl EJERCICIO 33")
+
+
+#Ejercicio 34
+gasto_general_variable=95.5
+type_gasto_general_variable=type(gasto_general_variable)
+
+gasto_general_variable_es_float=isinstance(gasto_general_variable,float)
+gasto_general_variable_es_str=isinstance(gasto_general_variable,str)
+
+print("Variable gasto general variable es:",type_gasto_general_variable)
+print("Variable es float?:",gasto_general_variable_es_float)
+print("Variable es str?:",gasto_general_variable_es_str)
+print("FIN DEl EJERCICIO 34")
+
+
+#Ejercicio 35
+utilidad=50
+type_utilidad=type(utilidad)
+
+utilidad_es_int=isinstance(utilidad,int)
+utilidad_es_str=isinstance(utilidad,str)
+
+print("Variable utilidad es:",type_utilidad)
+print("Variable es int?:",utilidad_es_int)
+print("Variable es str?:",utilidad_es_str)
+print("FIN DEl EJERCICIO 35")
+
+
+#Ejercicio 36
+subtotal=365.9
+type_subtotal=type(subtotal)
+
+subtotal_es_float=isinstance(subtotal,float)
+subtotal_es_bool=isinstance(subtotal,bool)
+
+print("Variable subtotal es:",type_subtotal)
+print("Variable es float?:",subtotal_es_float)
+print("Variable es bool?:",subtotal_es_bool)
+print("FIN DEl EJERCICIO 36")
+
+
+#Ejercicio 37
+descuento=0.5
+type_descuento=type(descuento)
+
+descuento_es_float=isinstance(descuento,float)
+descuento_es_int=isinstance(descuento,int)
+
+print("Variable descuento es:",type_descuento)
+print("Variable es float?:",descuento_es_float)
+print("Variable es int?:",descuento_es_int)
+print("FIN DEl EJERCICIO 37")
+
+
+#Ejercicio 38
+precio_original=80
+type_precio_original=type(precio_original)
+
+precio_original_es_int=isinstance(precio_original,int)
+precio_original_es_str=isinstance(precio_original,str)
+
+print("Variable precio original es:",type_precio_original)
+print("Variable es int?:",precio_original_es_int)
+print("Variable es str?:",precio_original_es_str)
+print("FIN DEl EJERCICIO 38")
+
+
+#Ejercicio 39
+precio_descuento=75.5
+type_precio_descuento=type(precio_descuento)
+
+precio_descuento_es_float=isinstance(precio_descuento,float)
+precio_descuento_es_int=isinstance(precio_descuento,int)
+
+print("Variable precio descuento es:",type_precio_descuento)
+print("Variable es float?:",precio_descuento_es_float)
+print("Variable es int?:",precio_descuento_es_int)
+print("FIN DEl EJERCICIO 39")
+
+
+#Ejercicio 40
+precio_final=100.5
+type_precio_final=type(precio_final)
+
+precio_final_es_float=isinstance(precio_final,float)
+precio_final_es_bool=isinstance(precio_final,bool)
+
+print("Variable precio final es:",type_precio_final)
+print("Variable es float?:",precio_final_es_float)
+print("Variable es bool?:",precio_final_es_bool)
+print("FIN DEl EJERCICIO 40")
+
+
+#Ejercicio 41
+Matematica=20
+type_Matematica=type(Matematica)
+
+Matematica_es_int=isinstance(Matematica,int)
+Matematica_es_bool=isinstance(Matematica,bool)
+
+print("Variable Matematica es:",type_Matematica)
+print("Variable es int?:",Matematica_es_int)
+print("Variable es bool?:",Matematica_es_bool)
+print("FIN DEl EJERCICIO 41")
+
+
+#Ejercicio 42
+credit_mat=6
+type_credit_mat=type(credit_mat)
+
+credit_mat_es_int=isinstance(credit_mat,int)
+credit_mat_es_str=isinstance(credit_mat,str)
+
+print("Variable creditos matemática es:",type_credit_mat)
+print("Variable es int?:",credit_mat_es_int)
+print("Variable es str?:",credit_mat_es_str)
+print("FIN DEl EJERCICIO 42")
+
+
+#Ejercicio 43
+geometria=14
+type_geometria=type(geometria)
+
+geometria_es_int=isinstance(geometria,int)
+geometria_es_bool=isinstance(geometria,bool)
+
+print("Variable geometria es:",type_geometria)
+print("Variable es int?:",geometria_es_int)
+print("Variable es bool?:",geometria_es_bool)
+print("FIN DEl EJERCICIO 43")
+
+
+#Ejercicio 43
+geometria=14
+type_geometria=type(geometria)
+
+geometria_es_int=isinstance(geometria,int)
+geometria_es_bool=isinstance(geometria,bool)
+
+print("Variable geometria es:",type_geometria)
+print("Variable es int?:",geometria_es_int)
+print("Variable es bool?:",geometria_es_bool)
+print("FIN DEl EJERCICIO 43")
+
+
+#Ejercicio 44
+credit_geo=14
+type_credit_geo=type(credit_geo)
+
+credit_geo_es_int=isinstance(credit_geo,int)
+credit_geo_es_float=isinstance(credit_geo,float)
+
+print("Variable creditos de geometría es:",type_credit_geo)
+print("Variable es int?:",credit_geo_es_int)
+print("Variable es float?:",credit_geo_es_float)
+print("FIN DEl EJERCICIO 44")
+
+
+#Ejercicio 45
+quimica=14
+type_quimica=type(quimica)
+
+quimica_es_int=isinstance(quimica,int)
+quimica_es_str=isinstance(quimica,str)
+
+print("Variable quimica es:",type_quimica)
+print("Variable es int?:",quimica_es_int)
+print("Variable es str?:",quimica_es_str)
+print("FIN DEl EJERCICIO 45")
+
+
+#Ejercicio 46
+credit_quim=3
+type_credit_quim=type(credit_quim)
+
+credit_quim_es_int=isinstance(credit_quim,int)
+credit_quim_es_bool=isinstance(credit_quim,bool)
+
+print("Variable creditos de química es:",type_credit_quim)
+print("Variable es int?:",credit_quim_es_int)
+print("Variable es bool?:",credit_quim_es_bool)
+print("FIN DEl EJERCICIO 46")
+
+
+#Ejercicio 47
+lenguaje=18
+type_lenguaje=type(lenguaje)
+
+lenguaje_es_int=isinstance(lenguaje,int)
+lenguaje_es_float=isinstance(lenguaje,float)
+
+print("Variable lenguaje es:",type_lenguaje)
+print("Variable es int?:",lenguaje_es_int)
+print("Variable es float?:",lenguaje_es_float)
+print("FIN DEl EJERCICIO 47")
+
+
+#Ejercicio 48
+credit_leng=4
+type_credit_leng=type(credit_leng)
+
+credit_leng_es_int=isinstance(credit_leng,int)
+credit_leng_es_str=isinstance(credit_leng,str)
+
+print("Variable creditos de lenguaje es:",type_credit_leng)
+print("Variable es int?:",credit_leng_es_int)
+print("Variable es str?:",credit_leng_es_str)
+print("FIN DEl EJERCICIO 48")
+
+
+#Ejercicio 49
+promedio_ponderado=17
+type_promedio_ponderado=type(promedio_ponderado)
+
+promedio_ponderado_es_int=isinstance(promedio_ponderado,int)
+promedio_ponderado_es_bool=isinstance(promedio_ponderado,bool)
+
+print("Variable promedio ponderado es:",type_promedio_ponderado)
+print("Variable es int?:",promedio_ponderado_es_int)
+print("Variable es bool?:",promedio_ponderado_es_bool)
+print("FIN DEl EJERCICIO 49")
+
+#Ejercicio 50
+x="a"
+type_x=type(x)
+
+x_es_str=isinstance(x,str)
+x_es_int=isinstance(x,int)
+
+print("Variable x es:",type_x)
+print("Variable es str?:",x_es_str)
+print("Variable es int?:",x_es_int)
+print("FIN DEl EJERCICIO 50")
